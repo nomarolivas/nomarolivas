5f12c4c7af44ab2b373161e5bd44fb8ed8ebf46c
Nomar Olivas <41343085+nomarolivas@users.noreply.github.com>
2024-12-18 20:00:10 -0800

    Home Page Complete

--- src/components/Nomar.js
+++ src/components/Nomar.js
@@ -5,23 +5,62 @@ import profilePicture from '../assets/profilePicture.JPG'
 
 const Nomar = () => {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-3 gap-8 max-w-[1200px] h-[70vh] mx-auto py-8 bg-black'>
+    <div className='grid grid-cols-1 sm:grid-cols-3 gap-8 max-w-[1200px] md:h-[70vh] mx-auto py-8 bg-black'>
 
-        <div className='my-auto mx-auto'>
+      <div className='col-span-1 my-auto mx-auto w-[300px] h-auto lg:w-[400px]'>
 
-            <div className='w-[300px] h-auto lg:w-[400px]'>
-                <img src={profilePicture} alt='Nomar profile picture'></img>
+        <img src={profilePicture} alt='Nomar profile picture' />
 
-            </div>
+      </div>
+
+      <div className='col-span-2 px-5 my-auto'>
+
+        <h1 className='text-white text-4xl sm:text-5xl lg:text-7xl font-extrabold'>
+
+          <span className="primary-color">
+            Nomar Olivas
+          </span> <br></br>
+
+          <TypeAnimation
+            sequence={[
+              "Data Analyst",
+              1000,
+              "IT Specialist",
+              1000,
+              "Software Engineer",
+              1000,
+            ]}
+
+            wrapper='span'
+            speed={50}
+            repeat={Infinity}
+          />
+        </h1>
+
+        <div className='my-8'>
+          <a href='/' className='px-6 py-3 w-full rounded-xl mr-4     
+            bg-gradient-to-br  from-blue-400 to-green-400 text-white'>
+            Resume
+          </a>
+
+          <a href='/' className='px-6 py-3 w-full rounded-xl mr-4     
+            border border-gray-400 hover:bg-gradient-to-br  from-blue-400 to-green-400
+             text-white hover:border-none'>
+            Contact
+          </a>
+
+
 
         </div>
 
-        <div className='col-span-2 px-5'>
-            
 
 
-        </div>
-      
+
+
+      </div>
+
+
+
     </div>
   )
 }
