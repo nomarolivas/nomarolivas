59d836c5f2186db4f177ba013c7ab7aeec933f56
Nomar Olivas <nomarolivas@gmail.com>
2025-03-05 20:45:24 -0800

    Added contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,33 +9,53 @@ const Contact = () => {
         </div>
 
         <div className='max-w-[800px] mx-auto'>
-            <div className='mt-6 bg-[161616] rounded-xl'>
+            <div className='mt-6 bg-[#161616] rounded-xl'>
                 <div className='p-10'>
+
                     <form action="#"  method='POST'>
-                        <div>
-                            <div className='mt-2.5 relative'>
-                                <input type="text" name='' id='' placeholder='Your Name'
-                                    className='bg-[161616] w-full px-4 py-4 text-gray-400 placeholder-gray-400 border
-                                        border-gray-700 rounded-md focus-md focus:outline-none focus:border-pink-600'/>
+                        <div className='grid grid-cols-1 sm:grid-cols-2 gap-x-5 gap-y-4'>
+                            <div>
+                                <div className='mt-2.5'>
+                                    <input type="text" name='name' id='' placeholder='Your Name'
+                                        class='bg-[#161616] w-full px-4 py-4 text-gray-400 placeholder-gray-400 border
+                                            border-gray-700 rounded-md focus-md focus:outline-none focus:border-pink-600'/>
+                                </div>
                             </div>
-                        </div>
 
-                        <div>
-                            <div className='mt-2.5 relative'>
-                                <input type="text" name='' id='' placeholder='Your Email'
-                                    className='bg-[161616] w-full px-4 py-4 text-gray-400 placeholder-gray-400 border
-                                        border-gray-700 rounded-md focus-md focus:outline-none focus:border-pink-600'/>
+                            <div>
+                                <div className='mt-2.5 relative'>
+                                    <input type="text" name='email' id='' placeholder='Your Email'
+                                        class='bg-[#161616] w-full px-4 py-4 text-gray-400 placeholder-gray-400 border
+                                            border-gray-700 rounded-md focus-md focus:outline-none focus:border-pink-600'/>
+                                </div>
                             </div>
-                        </div>
 
-                        <div>
-                            <div className='mt-2.5 relative'>
-                                <input type="text" name='' id='' placeholder='Your Message'
-                                    className='bg-[161616] w-full px-4 py-4 text-gray-400 placeholder-gray-400 border
-                                        border-gray-700 rounded-md focus-md focus:outline-none focus:border-pink-600'/>
+                            <div class="sm:col-span-2">
+                                <div className='mt-2.5 relative'>
+                                    <textarea  name='message' id='' placeholder='Your Message'
+                                        class="bg-[#161616] w-full px-4 py-4 text-gray-400 placeholder-gray-400 border
+                                            border-gray-700 rounded-md focus:outline-none focus:border-pink-600" rows="4"> 
+                                        </textarea>
+                                </div>
                             </div>
-                        </div>
 
+                            <div class="sm:col-span-2">
+                                <button type="submit"
+                                    class="text-xl w-full p-4 mt-2 font-semibold text-white bg-primary-color rounded-md">
+                                        Send
+
+                                </button>
+
+
+                            </div>
+
+
+
+
+
+
+
+                        </div>
 
 
 
